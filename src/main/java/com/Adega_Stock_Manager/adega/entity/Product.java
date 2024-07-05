3e8bc14028d7114b62a0f0087f9b27cb6fedd37e
Jonathas Menezes <jonathassk8@gmail.com>
2024-07-05 00:45:12 -0300

    Change Mysql to Mariadb. Configure the application.properties file.

--- src/main/java/com/Adega_Stock_Manager/adega/entity/Product.java
+++ src/main/java/com/Adega_Stock_Manager/adega/entity/Product.java
@@ -1,11 +1,18 @@
 package com.Adega_Stock_Manager.adega.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nome;
     private String cod_barra;
     private float valor;
